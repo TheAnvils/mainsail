2eff5e06da2778fc73b8710964b132d6fe604d0d
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2022-10-08 09:26:10 +0000

    add express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
 const { client } = require('./db.js');
+const express = require('express')
+const app = express()
+const port = 3000
 Sentry.init({
   dsn: process.env.DSN,
  tracesSampleRate: 1.0,
@@ -17,5 +20,11 @@ const transaction = Sentry.startTransaction({
     transaction.finish();
   }
 function init() {
-  
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 }
