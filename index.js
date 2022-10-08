0ba337dbca655513b0d7e4fdde54539e9799f50b
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2022-10-08 10:42:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-gobgib)(;
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+Sentry.init({
+  dsn: "",
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
